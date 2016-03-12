2a72e025a01372f0ace53e6a649a7c83cd74c4c1
Brett Cannon <brett@python.org>
2016-03-11 21:22:36 -0800

    Fix a major reformatting kerfuffle

--- lib/data_service/common.dart
+++ lib/data_service/common.dart
@@ -1,47 +1,106 @@
-library which_film.main;
+library which_film.data_service;
 
 import "dart:async";
+import "dart:convert";
 
 import "package:which_film/data_search.dart";
-import "package:which_film/data_service/common.dart";
 
-/// Common code for driving both the VM and web apps.
-void driver(TraktService client, Iterable<String> usernames,
-    void process(Iterable<ChosenMovie> movies)) {
-  var futureUserData = usernames.map(client.fetchData);
-  Future.wait(futureUserData).then((userDataIterable) {
-    var data = {};
-    for (UserData userData in userDataIterable) {
-      if (userData.watchlist.isEmpty) {
-        print("WARNING: empty watchlist for ${userData.username}");
-        print('');
-        continue;
-      } else if (userData.ratings.isEmpty) {
-        print("WARNING: no ratings for ${userData.username}");
-        print('');
-        continue;
-      }
-      updateMovies(data, userData.watchlist, WhyChosen.watchlist);
-      updateMovies(data, userData.ratings[8], WhyChosen.rating08);
-      updateMovies(data, userData.ratings[9], WhyChosen.rating09);
-      updateMovies(data, userData.ratings[10], WhyChosen.rating10);
-      userData.lastWatched.forEach((m, d) {
-        if (data.containsKey(m)) {
-          var movie = data[m];
-          if (movie.lastWatched == null) {
-            movie.lastWatched = d;
-          } else if (movie.lastWatched.isBefore(d)) {
-            movie.lastWatched = d;
-          }
-        }
-      });
+class UserData {
+  String username;
+  Set<Movie> watchlist;
+  Map<int, Set<Movie>> ratings;
+  Map<Movie, DateTime> lastWatched;
+
+  UserData(this.username, this.watchlist, this.ratings, this.lastWatched) {
+    // Guarantee that there is a set for every possible rating to avoid other
+    // code needing to check if a rating key exists.
+    for (var x = 1; x <= 10; x++) {
+      ratings.putIfAbsent(x, () => new Set());
+    }
+  }
+}
+
+/// A data service for trakt.tv.
+abstract class TraktService {
+  static final requestHeaders = {
+    "Content-Type": "application/json",
+    "trakt-api-version": "2",
+    "trakt-api-key":
+        "3b1469ddbeaedc0b1f8dacd8035baea4fddff31fef7441e46e1c7a1c87fe9408"
+  };
+
+  /// Abstracted network request.
+  Future<String> fetch(String url);
+
+  String watchlistUrl(String username) =>
+      "https://api-v2launch.trakt.tv/" + "users/${username}/watchlist/movies";
+  String ratingsUrl(String username) =>
+      "https://api-v2launch.trakt.tv/users/" + "${username}/ratings/movies";
+  String lastWatchedUrl(String username) =>
+      "https://api-v2launch.trakt.tv/" + "users/${username}/watched/movies";
+
+  Movie _makeMovie(Map jsonData) {
+    var slug = jsonData["ids"]["slug"];
+    var url = "https://trakt.tv/movies/${slug}";
+    return new Movie(jsonData["title"], jsonData["year"], url);
+  }
+
+  Future<Set<Movie>> watchlist(String username) async {
+    var responseText = await fetch(watchlistUrl(username));
+    if (responseText == null) {
+      return new Set();
+    }
+    var jsonData = JSON.decode(responseText);
+    var movies = new Set<Movie>();
+    for (var watchlistData in jsonData) {
+      movies.add(_makeMovie(watchlistData["movie"]));
     }
 
-    var thisYear = (new DateTime.now()).year;
-    var acceptableMovies = data.values
-        .where((m) => m.numberOfReasons > 1 && m.year <= thisYear)
-        .toList();
-    acceptableMovies.sort((x, y) => x.compareTo(y) * -1);
-    process(acceptableMovies);
-  });
+    return movies;
+  }
+
+  Future<Map<int, Set<Movie>>> ratings(String username) async {
+    var responseText = await fetch(ratingsUrl(username));
+    if (responseText == null) {
+      return new Map();
+    }
+    var jsonData = JSON.decode(responseText);
+    var ratings = new Map<int, Set<Movie>>();
+    for (var ratingData in jsonData) {
+      var rating = ratingData["rating"];
+      var movie = _makeMovie(ratingData["movie"]);
+      ratings.putIfAbsent(rating, () => new Set()).add(movie);
+    }
+
+    return ratings;
+  }
+
+  Future<Map<Movie, DateTime>> lastWatched(String username) async {
+    var responseText = await fetch(lastWatchedUrl(username));
+    if (responseText == null) {
+      return new Map();
+    }
+    var jsonData = JSON.decode(responseText);
+    var lastWatchedMap = new Map<Movie, DateTime>();
+    for (var lastWatchedData in jsonData) {
+      var lastWatched = DateTime.parse(lastWatchedData["last_watched_at"]);
+      var movie = _makeMovie(lastWatchedData["movie"]);
+      lastWatchedMap[movie] = lastWatched;
+    }
+
+    return lastWatchedMap;
+  }
+
+  Future<UserData> fetchData(String username) async {
+    // It would be more optimal to fan-out all of these calls individually
+    // and then fan-in so that e.g., if you are making requests for two
+    // people then you are making 6 concurrent requests instead only two
+    // at a time. Trick is reconciling the responses back to users
+    // (although maybe that isn't important in the end?).
+    var watchlistData = await watchlist(username);
+    var ratingsData = await ratings(username);
+    var lastWatchedData = await lastWatched(username);
+
+    return new UserData(watchlistData, ratingsData, lastWatchedData);
+  }
 }
