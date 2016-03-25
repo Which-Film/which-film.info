5fa5bf9c7f60736c47cbdba9260340035490aea6
Brett Cannon <brett@python.org>
2016-03-25 10:25:50 -0700

    Log info in the browser as well as the CLI app.
    Closes #2

--- bin/which-film.dart
+++ bin/which-film.dart
@@ -7,11 +7,6 @@ import "package:which_film/data_service/vm.dart";
 import "package:which_film/main.dart";
 
 void main(List<String> args) {
-  Logger.root.level = Level.INFO;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.message}');
-  });
-
   var client = new TraktVmService();
   void process(Iterable<ChosenMovie> movies) {
     print("");
