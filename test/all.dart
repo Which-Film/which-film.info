3a126f052e7b81d5dff2ac110900b3802c7e1bff
Brett Cannon <brettcannon@users.noreply.github.com>
2016-07-11 10:08:34 -0700

    Add processing_test.dart to all.dart

--- test/all.dart
+++ test/all.dart
@@ -2,8 +2,10 @@ library all_test;
 
 import "data_search_test.dart" as data_search_test;
 import "data_service_test.dart" as data_service_test;
+import "processing_test.dart" as processing_test;
 
 main() {
   data_search_test.main();
   data_service_test.main();
+  processing_test.main();
 }
